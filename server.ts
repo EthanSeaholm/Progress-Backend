4cc70834b8876fc9a51e2566704069f965cab7c9
Ethan Wolfgang Seaholm <71782610+ethanseaholm@users.noreply.github.com>
2024-05-20 19:34:38 -0500

    cors troubleshooting

--- server.ts
+++ server.ts
@@ -26,4 +26,11 @@ mongoose.connect(env.MONGODB_URI)
         console.error("Error connecting to the database: ", error);
     });
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://www.prog-ress.live");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 export default app;
